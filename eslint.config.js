793329fd354cd592393b12eedef9f37f64f72731
DeaBruces <deannybruces@gmail.com>
2025-09-19 17:37:15 -0300

    chore: update package.json to include typescript-eslint and clean up imports in various components
    - Added "typescript-eslint" to dependencies in package.json.
    - Removed unused social media icon imports from Footer.astro and SocialIcons.astro.
    - Changed variable declarations from `let` to `const` in Formulario.astro, improving code clarity.
    - Removed unused import from Hero.astro.
    - Removed actualPage variable from Navigation.astro as it was not used.
    - Cleaned up 404.astro by removing tabindex from the heading.
    - Updated disqus_config variable declaration to const in blog post page for consistency.
    - Added title attribute to Google Tag Manager iframe for accessibility.

--- eslint.config.js
+++ eslint.config.js
@@ -1,12 +1,40 @@
 import eslintPluginAstro from "eslint-plugin-astro";
+import astroParser from "astro-eslint-parser";
+import tseslint from "typescript-eslint";
+
 export default [
+  {
+    ignores: [
+      ".astro/**",
+      ".frontmatter/**",
+      ".vercel/**",
+      ".vscode/**",
+      "dist/**",
+      "node_modules/**",
+      "**/*.d.ts", // Ignore generated TypeScript declaration files
+    ],
+  },
   // add more generic rule sets here, such as:
   // js.configs.recommended,
   ...eslintPluginAstro.configs.recommended,
+  ...eslintPluginAstro.configs["jsx-a11y-strict"],
+  ...tseslint.configs.recommended,
   {
+    files: ["**/*.astro"],
+    plugins: {
+      astro: eslintPluginAstro,
+    },
+    languageOptions: {
+      parser: astroParser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        extraFileExtensions: [".astro"],
+      },
+    },
     rules: {
-      // override/add rules settings here, such as:
-      // "astro/no-set-html-directive": "error"
+      ...eslintPluginAstro.configs.recommended.rules,
+      ...eslintPluginAstro.configs["jsx-a11y-strict"].rules,
+      "no-mixed-spaces-and-tabs": ["error", "smart-tabs"],
     },
   },
 ];
